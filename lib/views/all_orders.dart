a4b615d6b679b307c0e210945af45d8c184c610a
Alan-Ovalle <alanleyovalle@gmail.com>
2023-10-18 23:10:20 -0600

    PDF creation succesfully added

--- lib/views/all_orders.dart
+++ lib/views/all_orders.dart
@@ -5,6 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:techtruck_v11/views/new_order.dart';
 import 'package:techtruck_v11/widgets/db_helper.dart';
 import 'package:techtruck_v11/widgets/helper_widgets.dart';
+import 'package:techtruck_v11/api/pdf_api.dart';
+import 'package:techtruck_v11/api/pdf_invoice_api.dart';
+import 'package:techtruck_v11/model/customer.dart';
+import 'package:techtruck_v11/model/invoice.dart';
+import 'package:techtruck_v11/model/supplier.dart';
+import 'package:techtruck_v11/widgets/button_widget.dart';
+import 'package:techtruck_v11/widgets/title_widget.dart';
 
 class AllOrders extends StatefulWidget {
   const AllOrders({Key? key}) : super(key: key);
@@ -348,8 +355,82 @@ class _AllOrdersState extends State<AllOrders> {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
+                        onPressed: () async {
+                          final date = DateTime.now();
+                          final dueDate = date.add(Duration(days: 7));
+
+                          final invoice = Invoice(
+                            supplier: Supplier(
+                              name: 'Sarah Field',
+                              address: 'Sarah Street 9, Beijing, China',
+                              paymentInfo: 'https://paypal.me/sarahfieldzz',
+                            ),
+                            customer: Customer(
+                              name: 'Apple Inc.',
+                              address: 'Apple Street, Cupertino, CA 95014',
+                            ),
+                            info: InvoiceInfo(
+                              date: date,
+                              dueDate: dueDate,
+                              description: 'My description...',
+                              number: '${DateTime.now().year}-9999',
+                            ),
+                            items: [
+                              InvoiceItem(
+                                description: 'Coffee',
+                                date: DateTime.now(),
+                                quantity: 3,
+                                vat: 0.19,
+                                unitPrice: 5.99,
+                              ),
+                              InvoiceItem(
+                                description: 'Water',
+                                date: DateTime.now(),
+                                quantity: 8,
+                                vat: 0.19,
+                                unitPrice: 0.99,
+                              ),
+                              InvoiceItem(
+                                description: 'Orange',
+                                date: DateTime.now(),
+                                quantity: 3,
+                                vat: 0.19,
+                                unitPrice: 2.99,
+                              ),
+                              InvoiceItem(
+                                description: 'Apple',
+                                date: DateTime.now(),
+                                quantity: 8,
+                                vat: 0.19,
+                                unitPrice: 3.99,
+                              ),
+                              InvoiceItem(
+                                description: 'Mango',
+                                date: DateTime.now(),
+                                quantity: 1,
+                                vat: 0.19,
+                                unitPrice: 1.59,
+                              ),
+                              InvoiceItem(
+                                description: 'Blue Berries',
+                                date: DateTime.now(),
+                                quantity: 5,
+                                vat: 0.19,
+                                unitPrice: 0.99,
+                              ),
+                              InvoiceItem(
+                                description: 'Lemon',
+                                date: DateTime.now(),
+                                quantity: 4,
+                                vat: 0.19,
+                                unitPrice: 1.29,
+                              ),
+                            ],
+                          );
+
+                          final pdfFile = await PdfInvoiceApi.generate(invoice);
+
+                          PdfApi.openFile(pdfFile);
                         },
                         icon: const Icon(Icons.print),
                         color: Colors.blueGrey,
---
+++ lib/views/pdf_order.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:printing/printing.dart';
+
+class PdfOrder extends StatelessWidget {
+  const PdfOrder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
